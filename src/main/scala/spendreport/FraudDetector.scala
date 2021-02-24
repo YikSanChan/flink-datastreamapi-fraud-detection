03813b60735999f2ce7b42ca34ecb0d6c46e7ab9
陈易生 <chenyisheng14418@ipalfish.com>
2021-02-24 21:33:58 +0800

    v1: if an account sees a small-amount tx followed by a large-amount tx

--- src/main/scala/spendreport/FraudDetector.scala
+++ src/main/scala/spendreport/FraudDetector.scala
@@ -18,6 +18,9 @@
 
 package spendreport
 
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
+import org.apache.flink.api.scala.typeutils.Types
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
 import org.apache.flink.walkthrough.common.entity.Alert
@@ -35,15 +38,39 @@ object FraudDetector {
 @SerialVersionUID(1L)
 class FraudDetector extends KeyedProcessFunction[Long, Transaction, Alert] {
 
+  @transient private var flagState: ValueState[java.lang.Boolean] = _
+
+  @throws[Exception]
+  override def open(parameters: Configuration): Unit = {
+    val flagDescriptor = new ValueStateDescriptor("flag", Types.BOOLEAN)
+    flagState = getRuntimeContext.getState(flagDescriptor)
+  }
+
   @throws[Exception]
   def processElement(
       transaction: Transaction,
       context: KeyedProcessFunction[Long, Transaction, Alert]#Context,
       collector: Collector[Alert]): Unit = {
 
-    val alert = new Alert
-    alert.setId(transaction.getAccountId)
+    // Get the current state for the current key
+    val lastTransactionWasSmall = flagState.value
 
-    collector.collect(alert)
+    // Check if the flag is set
+    if (lastTransactionWasSmall != null) {
+      if (transaction.getAmount > FraudDetector.LARGE_AMOUNT) {
+        // Output an alert downstream
+        val alert = new Alert
+        alert.setId(transaction.getAccountId)
+
+        collector.collect(alert)
+      }
+      // Clean up our state
+      flagState.clear()
+    }
+
+    if (transaction.getAmount < FraudDetector.SMALL_AMOUNT) {
+      // set the flag to true
+      flagState.update(true)
+    }
   }
 }
